f80b9125f76586a103edf84b299f2ad7583b6e50
蒋震震 <xiaoyang41163@qq.com>
2015-08-11 14:03:20 +0800

    商品分类的js
    。。。

---
+++ Public/script/goodsclassify.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$(".moveleft").click(function(){
+		var lef=$(".goodsclassify-header ul").css("left");
+		if(parseInt(lef)<0){
+		$(".goodsclassify-header ul").css( "left", function(index, value) {return parseFloat(value)+1*$(".goodsclassify-header li").width();});
+		}
+	})
+	
+	$(".moveright").click(function(){
+		var lef=$(".goodsclassify-header ul").css("left");
+		var cou=$("#classifynavi>li").length;
+		var wid=(cou-4)*$(".goodsclassify-header li").width();
+		if(-parseInt(lef)<parseInt(wid)){
+		$(".goodsclassify-header ul").css("left", function(index, value) {return parseFloat(value)-1*$(".goodsclassify-header li").width();});
+		}
+	})
+	
+	$("#classifynavi li").click(function(){
+		$("#classifynavi li").each(function(){
+               $(this).removeClass("goodsclassify-active");
+            });
+		$(this).addClass("goodsclassify-active");
+	});
+	
+	
+	
+	
+})
